f610eb25cd5dc679e1516f9b384898b591aaa0cf
jaenam615 <jaenam2060@gmail.com>
2024-10-22 18:29:29 +0900

    jdbc and jdbc template

--- src/main/java/hello/hello_spring/controller/MemberController.java
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hello_spring.domain.Member;
 import hello.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -23,11 +26,18 @@ public class MemberController {
 
     @PostMapping("/members/new")
     public String create(MemberForm form) {
-        Member member = new Member();
-        member.setName(form.getName());
+        Member member = new Member(); // 멤버 인스턴스 새로 생성
+        member.setName(form.getName()); // 이름 설정
 
-        memberService.join(member);
+        memberService.join(member); // 생성한 멤버를 저장
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
